d7d30a661490ae113e3540db95a424f4bf534dd7
Edward Boreham <34766382+Dogwar266@users.noreply.github.com>
2020-02-25 00:31:07 +1100

    Fixed it all up yay

--- roleBot.js
+++ roleBot.js
@@ -58,17 +58,10 @@ client.on('ready', () => {
     client.user.setActivity('Coded by Dogwar#2002');
 });
 
-let channel_id = "650567782890471436";
-let message_id = "651598494141775872";
+
 
 client.on("ready", (reaction, user) => {
 
-    client.channels.get(channel_id).fetchMessage(message_id).then(m => {
-        console.log("Cached reaction message.");
-    }).catch(e => {
-        console.error("Error loading message.");
-        console.error(e);
-    });
 
     client.on('message', message => {
         if (!message.content.startsWith(prefix) || message.author.bot) return;
